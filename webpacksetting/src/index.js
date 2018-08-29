2f77239fabbd6421981f81124f1dd553c7e0d944
ByeonGi <ghgh2090@gmail.com>
2018-08-29 14:21:15 +0900

    USE Webpack try : 2

--- webpacksetting/src/index.js
+++ webpacksetting/src/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 
-const port = process.env.PORT || 3030
-const app = express()
+const port = process.env.PORT || 3030;
+const app = express();
 
 app.get('/', (req, res) =>{
     res.send('hello world');
 })
+app.listen(port, () => console.log('listening on port ${port}'));
 
-app.listen(port, ()=>console.log('listening on port ${port}'))
--- webpacksetting/webpack.config.js
+++ webpacksetting/webpack.config.js
@@ -1,6 +1,9 @@
 const nodeExternals = require('webpack-node-externals');
 
-moduls.exports = {
-    target : 'node',
+module.exports = {
+    target:'node',
     externals : [nodeExternals()],
 }
+
+//webpack.config.js 가 아닌 이름의 파일을 설정 파일로 쓰고 싶다면
+//"build" : "webpack --mode development --config 설정 파일명"
